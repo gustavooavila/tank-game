57d3041ece09d0ec45ca9a6d060cb6959f11e9ea
Shadownrun <gustavooavila@gmail.com>
2020-09-19 17:00:50 -0300

    created a class for each command

--- src/js/Commands.js
+++ src/js/Commands.js
@@ -1,19 +1,7 @@
-class Command {
-    constructor(command, sortable){
-        this.e = $(`<div class="command">`);
-        this.e.attr("data-command", command);
-        this.e.draggable({
-            helper: "clone",
-            connectToSortable: sortable,
-        });
-        this.e.click(function(){sortable.append($(this).clone())});
-    }
-    appendTo(container){
-        this.e.appendTo(container);
-        return this;
-    }
-    
-};
+const commandManager = {
+    commands: {},
+    registerCommand: function(command,object){this.commands[command] = object;}
+}
 
 class commandStack{
     constructor(){
@@ -33,15 +21,100 @@ class commandStack{
             }
         });
     }
+    
     appendTo(container){
         this.e.appendTo(container);
         return this;
     }
+    
     getCommands(){
         return this.e.sortable("toArray", {attribute: "data-command"});
     }
+    
     getElement(){
         return this.e;
     }
     
+}
+
+class Command {
+    constructor(command, sortable){
+        this.e = $(`<div class="command">`);
+        this.e.attr("data-command", command);
+        this.e.draggable({
+            helper: "clone",
+            connectToSortable: sortable,
+        });
+        this.e.click(function(){sortable.append($(this).clone())});
+        
+        commandManager.registerCommand(command, this);
+    }
+    
+    appendTo(container){
+        this.e.appendTo(container);
+        return this;
+    }
+    
+    action(gameEntity, stage){
+        // false makes the command be executed again, by making the loop not increment the command count;
+        return true;
+    }
+};
+
+class moveForward extends Command{
+    constructor(sortable){
+        super("moveForward", sortable);
+    }
+    action(gameEntity, stage){
+        if(!stage.checkCollisionForward(tank.x, tank.y, tank.rotation)) tank.moveForward();
+        return true;
+    }
+    
+}
+
+class rotateClockwise extends Command{
+    constructor(sortable){
+        super("rotateClockwise", sortable);
+    }
+    action(gameEntity){
+        tank.rotateClockwise()
+        return true;
+    }
+    
+}
+
+class rotateAnticlockwise extends Command{
+    constructor(sortable){
+        super("rotateAnticlockwise", sortable);
+    }
+    action(gameEntity){
+        tank.rotateAnticlockwise()
+        return true;
+    }
+    
+}
+
+class shoot extends Command{
+    constructor(sortable){
+        super("shoot", sortable);
+        this.bullet = null;
+    }
+    createBullet(gameEntity, stage){
+        if(gameEntity.stop) gameEntity.stop();
+        this.bullet = new Bullet(gameEntity.x, gameEntity.y, gameEntity.rotation);
+        this.bullet.appendTo(stage.e);
+    }
+    action(gameEntity, stage){
+        if(!this.bullet){
+            this.createBullet(gameEntity, stage);
+            return false;
+        }
+        if(checkCollisionForward.call(stage, this.bullet.x, this.bullet.y, this.bullet.rotation)){
+            this.bullet.remove();
+            this.bullet = null;
+            return true;
+        }
+        this.bullet.moveForward();
+        return false;
+    }
 }
