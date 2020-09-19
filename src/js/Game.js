174b65747388466a4541c74b457ad218b2d3b12e
Shadownrun <gustavooavila@users.noreply.github.com>
2020-09-19 19:14:07 -0300

    Revert "Command manager"

--- src/js/Game.js
+++ src/js/Game.js
@@ -4,6 +4,7 @@ $(function(){
         stageData: null,
         commandStack: null,
         bullet: null, 
+        commands: [],
         controls: [],
         
         createStage: function(stage, container){
@@ -19,10 +20,10 @@ $(function(){
         },
         
         createCommands: function(container){
-            new rotateAnticlockwise(this.commandStack.getElement()).appendTo(container)
-            new moveForward(this.commandStack.getElement()).appendTo(container)
-            new rotateClockwise(this.commandStack.getElement()).appendTo(container)
-            new shoot(this.commandStack.getElement()).appendTo(container)
+            this.commands.push(new Command("rotate-anticlockwise", this.commandStack.getElement()).appendTo(container));
+            this.commands.push(new Command("move-forward", this.commandStack.getElement()).appendTo(container));
+            this.commands.push(new Command("rotate-clockwise", this.commandStack.getElement()).appendTo(container));
+            this.commands.push(new Command("shoot", this.commandStack.getElement()).appendTo(container));
         },
         
         createControls: function(container){
