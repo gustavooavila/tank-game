f5c1ebe9641e564ba9454b3df1edb0bdcdacdcb5
Shadownrun <gustavooavila@gmail.com>
2021-09-17 18:22:34 -0300

    update constraint so that it works when zoomed in

--- src/js/Game/PanZoomControls.js
+++ src/js/Game/PanZoomControls.js
@@ -1,18 +1,15 @@
 class PanZoomControls extends EventTarget{
     constructor(controlled, container){
         super();
-        this.scale = 1;
-        this.posX = 0;
-        this.posY  = 0;
-        
         this.controlled = controlled;
+        
         this.e = $("<div class='panZoomControls'></div>");
-        this.Panzoom = Panzoom(controlled);
-        this.Panzoom.setOptions({ maxScale: 1});
-        
-        
-        
         this.e.appendTo(container);
+        
+        this.Panzoom = Panzoom(controlled);
+        this.Panzoom.setOptions();
+        
+        this.center();
         
         this.constrainEvents();
         this.listenTouchpadZoom();        
@@ -20,7 +17,7 @@ class PanZoomControls extends EventTarget{
     
     listenTouchpadZoom(){
         window.addEventListener("wheel", (e)=>{
-            if(!e.ctrlKey)return;
+            //if(!e.ctrlKey)return;
             this.Panzoom.zoomWithWheel(e);
         }, {passive: false});
     }
@@ -30,7 +27,6 @@ class PanZoomControls extends EventTarget{
             const {x, y, scale} = e.detail;
             const result = this.constrainXY(x, y, scale)
             this.Panzoom.pan(result.x, result.y ,{silent: true, force: true})
-            console.log('is it looping???')
         });
         
     }
@@ -44,37 +40,59 @@ class PanZoomControls extends EventTarget{
         toY = parseFloat(toY)
         
         const dims = getDimensions(this.controlled);
-        const realWidth = dims.elem.width / scale
-        const realHeight = dims.elem.height / scale
+        const realWidth = this.controlled.offsetWidth
+        const realHeight = this.controlled.offsetHeight
         const scaledWidth = realWidth * toScale
         const scaledHeight = realHeight * toScale
         const diffHorizontal = (scaledWidth - realWidth) / 2
         const diffVertical = (scaledHeight - realHeight) / 2
         
-        const minX = (-dims.elem.margin.left - dims.parent.padding.left + diffHorizontal) / toScale
-        const maxX =
-        (dims.parent.width -
-            scaledWidth -
-            dims.parent.padding.left -
-            dims.elem.margin.left -
-            dims.parent.border.left -
-            dims.parent.border.right +
-        diffHorizontal) /
-        toScale
-        result.x = Math.max(Math.min(result.x, maxX), minX)
+        const containerWidth = dims.parent.width;
+        const containerHeight = dims.parent.height;
         
-        const minY = (-dims.elem.margin.top - dims.parent.padding.top + diffVertical) / toScale
-        const maxY =
-        (dims.parent.height -
-            scaledHeight -
-            dims.parent.padding.top -
-            dims.elem.margin.top -
-            dims.parent.border.top -
-            dims.parent.border.bottom +
-        diffVertical) /
-        toScale
-        result.y = Math.max(Math.min(result.y, maxY), minY)
+        
+        if(scaledWidth > containerWidth){
+            const extraWidth = Math.max((scaledWidth - dims.parent.width), 0);
+            
+            const minX = (-dims.elem.margin.left - dims.parent.padding.left - extraWidth + diffHorizontal) / toScale;
+            const maxX = (diffHorizontal -dims.parent.padding.left - dims.elem.margin.left - dims.parent.border.left - dims.parent.border.right) / toScale;
+            
+            result.x = Math.max(Math.min(result.x, maxX), minX);
+        }
+        else
+        {
+            const minX = (-dims.elem.margin.left - dims.parent.padding.left + diffHorizontal) / toScale
+            const maxX = (dims.parent.width - scaledWidth - dims.parent.padding.left - dims.elem.margin.left - dims.parent.border.left - dims.parent.border.right + diffHorizontal) / toScale 
+            result.x = Math.max(Math.min(result.x, maxX), minX)
+        }
+        
+        if(scaledHeight > containerHeight){
+            const extraHeight = Math.max((scaledHeight - dims.parent.height), 0);
+            
+            const minY = (-dims.elem.margin.top - dims.parent.padding.top - extraHeight + diffVertical) / toScale 
+            const maxY = (diffVertical - dims.parent.padding.top - dims.elem.margin.top - dims.parent.border.top - dims.parent.border.bottom) / toScale
+            
+            result.y = Math.max(Math.min(result.y, maxY), minY)
+        }
+        else{
+            const minY = (-dims.elem.margin.top - dims.parent.padding.top + diffVertical) / toScale 
+            const maxY = (dims.parent.height - scaledHeight - dims.parent.padding.top - dims.elem.margin.top - dims.parent.border.top - dims.parent.border.bottom + diffVertical) / toScale
+            
+            result.y = Math.max(Math.min(result.y, maxY), minY)
+        }     
+        
         return result
         
+    }
+    
+    center(){        
+        const dims = getDimensions(this.controlled);
+        
+        const elemCenter = {x: dims.elem.width / 2, y: dims.elem.height / 2};
+        const parentCenter = {x: dims.parent.width / 2, y: dims.parent.height / 2};
+        
+        const centerDiff = {x: parentCenter.x - elemCenter.x, y: parentCenter.y - elemCenter.y};
+        
+        this.Panzoom.pan(centerDiff.x, centerDiff.y ,{silent: true, force: true});
     }
 }
--- src/js/Game/PanZoomControlsUtils.js
+++ src/js/Game/PanZoomControlsUtils.js
@@ -13,24 +13,32 @@ function getDimensions(elem) {
     return {
         elem: {
             style,
+            
             width: rectElem.width,
             height: rectElem.height,
+            
             top: rectElem.top,
             bottom: rectElem.bottom,
             left: rectElem.left,
             right: rectElem.right,
+            
+            padding: getBoxStyle(elem, 'padding', style),
             margin: getBoxStyle(elem, 'margin', style),
             border: getBoxStyle(elem, 'border', style)
         },
         parent: {
             style: parentStyle,
+            
             width: rectParent.width,
             height: rectParent.height,
+            
             top: rectParent.top,
             bottom: rectParent.bottom,
             left: rectParent.left,
             right: rectParent.right,
+            
             padding: getBoxStyle(parent, 'padding', parentStyle),
+            margin: getBoxStyle(parent, 'margin', parentStyle),
             border: getBoxStyle(parent, 'border', parentStyle)
         }
     }
@@ -40,43 +48,43 @@ const prefixes = ['webkit', 'moz', 'ms'];
 const prefixCache = {};
 let divStyle
 function getBoxStyle(elem, name, style) {
-  if(!style) style = window.getComputedStyle(elem);
-  // Support: FF 68+
-  // Firefox requires specificity for border
-  const suffix = name === 'border' ? 'Width' : ''
-  return {
-    left: getCSSNum(`${name}Left${suffix}`, style),
-    right: getCSSNum(`${name}Right${suffix}`, style),
-    top: getCSSNum(`${name}Top${suffix}`, style),
-    bottom: getCSSNum(`${name}Bottom${suffix}`, style)
-  }
+    if(!style) style = window.getComputedStyle(elem);
+    // Support: FF 68+
+    // Firefox requires specificity for border
+    const suffix = name === 'border' ? 'Width' : ''
+    return {
+        left: getCSSNum(`${name}Left${suffix}`, style),
+        right: getCSSNum(`${name}Right${suffix}`, style),
+        top: getCSSNum(`${name}Top${suffix}`, style),
+        bottom: getCSSNum(`${name}Bottom${suffix}`, style)
+    }
 }
 
 function getCSSNum(name, style) {
-  return parseFloat(style[getPrefixedName(name)]) || 0
+    return parseFloat(style[getPrefixedName(name)]) || 0
 }
 
 function getPrefixedName(name) {
-  if (prefixCache[name]) {
-    return prefixCache[name]
-  }
-  const divStyle = createStyle()
-  if (name in divStyle) {
-    return (prefixCache[name] = name)
-  }
-  const capName = name[0].toUpperCase() + name.slice(1)
-  let i = prefixes.length
-  while (i--) {
-    const prefixedName = `${prefixes[i]}${capName}`
-    if (prefixedName in divStyle) {
-      return (prefixCache[name] = prefixedName)
+    if (prefixCache[name]) {
+        return prefixCache[name]
     }
-  }
+    const divStyle = createStyle()
+    if (name in divStyle) {
+        return (prefixCache[name] = name)
+    }
+    const capName = name[0].toUpperCase() + name.slice(1)
+    let i = prefixes.length
+    while (i--) {
+        const prefixedName = `${prefixes[i]}${capName}`
+        if (prefixedName in divStyle) {
+            return (prefixCache[name] = prefixedName)
+        }
+    }
 }
 function createStyle() {
-  if (divStyle) {
-    return divStyle
-  }
-  return (divStyle = document.createElement('div').style)
+    if (divStyle) {
+        return divStyle
+    }
+    return (divStyle = document.createElement('div').style)
 }
 
